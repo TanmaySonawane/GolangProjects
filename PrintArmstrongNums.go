defae35d4dc0756dbbf3c8d98878da904e86b3d1
TanmaySonawane <77171792+TanmaySonawane@users.noreply.github.com>
2023-09-21 17:58:45 -0400

    Update and rename ArmstrongNums.go to PrintArmstrongNums.go

--- PrintArmstrongNums.go
+++ PrintArmstrongNums.go
@@ -1,4 +1,4 @@
-// given a range of numbers from a to be, this code prints out all the armstring numbers that fall within that range
+// given a range of numbers from a to b, this code prints out all the armstrong numbers that fall within that range
 // An Armstring number is one where each digit raised to the power of the number of digits, when added with the rest of the digits 
 //(that were also raised to the same power), equals the same number, for example 153 = 1^3+5^3+3^3
 package main

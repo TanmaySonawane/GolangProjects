bf491609033ebd7a00ea7e1e479db0e811ec8229
TanmaySonawane <77171792+TanmaySonawane@users.noreply.github.com>
2023-09-21 20:45:56 -0400

    Update and rename perfectNum.go1 to perfectNum.go

--- perfectNum.go
+++ perfectNum.go
@@ -1,3 +1,5 @@
+// checks if the given number is a perfect number
+// a perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself
 package main
 
 import "fmt"
